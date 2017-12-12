7c9da2c19530930020e7f338e24d698d2da3857f
Nawwa Val <valentin.pezon@gmail.com>
2017-12-12 10:58:25 +0100

    First and maybe last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'app';
+  users: Array<any>;
+
+  constructor(private _dataservice: DataService) {
+    this._dataservice.getUsers()
+      .subscribe((rcv) => {
+        this.users = rcv;
+      });
+  }
 }
